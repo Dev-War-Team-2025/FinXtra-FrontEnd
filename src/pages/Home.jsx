2479995166889258f7e9d1c66a41762ce5db4608
Ako Junior <ngohako@gmail.com>
2025-03-28 01:06:08 -0300

    Refactor routing in App component and enhance Home page layout with new sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+// src/Home.js
+
+import React from 'react';
+import Header from './components/header';
+import JobCategories from './components/JobCategories';
+import JobListings from './components/JobListings';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+import Features from './components/features';
+import HeroSection from './components/heroSection';
+import AboutUs from './components/aboutus';
+import HowItWorks from './components/howItwork';
+import FAQ from './components/faq';
+import CTA from './components/cta';
+//import { Features } from 'tailwindcss';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container mx-auto">
+      <Header />
+      <HeroSection />
+      <Features /> 
+      <AboutUs />
+      <Testimonials />
+      <HowItWorks />
+      <CTA />
+      <FAQ /> 
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
