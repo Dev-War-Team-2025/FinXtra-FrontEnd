e1c035a6479fe8ddb2a98ff7261f3df2b325bad7
Ako Junior <ngohako@gmail.com>
2025-03-28 01:57:59 -0300

    Update package.json dependencies, refactor routing to Home component, and enhance Home page with Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Logo from './Logo'
+import FreeNavbar from '../Dashboard/Freelancer/FreeNavbar'
 
 const Navbar = () => {
   return (
     <>
-    <Logo />
+    <Logo />   
     </>
   )
 }
