9add2f29d5e8b05c1d90a247b0c0d7a3becc3463
Ako Junior <ngohako@gmail.com>
2025-03-28 03:12:06 -0300

    Fix import paths in Home component for consistent directory structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 // src/Home.js
 
 import React from 'react';
-import Header from './components/header';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
-import Features from './components/features';
-import HeroSection from './components/heroSection';
-import AboutUs from './components/aboutus';
-import HowItWorks from './components/howItwork';
-import FAQ from './components/faq';
-import CTA from './components/cta';
+import Header from '../components/header';
+import Testimonials from '../components/Testimonials';
+import Footer from '../components/Footer';
+import Features from '../components/features';
+import HeroSection from '../components/heroSection';
+import AboutUs from '../components/aboutus';
+import HowItWorks from '../components/howItwork';
+import FAQ from '../components/faq';
+import CTA from '../components/cta';
 //import { Features } from 'tailwindcss';
 
 const Home = () => {
